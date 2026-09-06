fa30e98dacd1a4bdac7501e65c8c3306f0981e4c
PHAM PHUONG THY <phamphuongthy2005@gmail.com>
2026-09-06 10:18:13 +0700

    Update 0. overview.sql

--- sql/0. overview.sql
+++ sql/0. overview.sql
@@ -1,25 +1,16 @@
 -- =========================================================
 -- 0. OVERVIEW
--- Grain: Entire dataset
 -- =========================================================
-
 SELECT
     ROUND(SUM(Revenue), 2) AS TotalRevenue,
-
     COUNT(DISTINCT TransactionNumber) AS TotalOrders,
-
     SUM(Quantity) AS TotalQuantity,
-
     ROUND(
         SAFE_DIVIDE(
             SUM(Revenue),
             COUNT(DISTINCT TransactionNumber)
-        ),
-        2
+        ), 2
     ) AS AOV,
-
     ROUND(AVG(Discount), 2) AS AvgDiscount,
-
     COUNT(DISTINCT ProductID) AS NumberOfProducts
-
 FROM `sales-of-fmcg-stores.salesfmcg.vw_sales_analysis`;
