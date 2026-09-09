b3a9ab6ba93240e8c4a47cd0d0a5dd94a4bc733a
PHAM PHUONG THY <phamphuongthy2005@gmail.com>
2026-09-09 10:41:10 +0700

    Update 4. employee_performance.sql

--- sql/4. employee_performance.sql
+++ sql/4. employee_performance.sql
@@ -5,12 +5,7 @@ SELECT
     SalesPersonID,
     COUNT(DISTINCT TransactionNumber) AS NumberOfInvoices,
     ROUND(SUM(Revenue), 2) AS TotalSalesRevenue,
-    ROUND(
-        SAFE_DIVIDE(
-            SUM(Revenue),
-            COUNT(DISTINCT TransactionNumber)
-        ), 2
-    ) AS AverageRevenuePerInvoice
+    ROUND(SAFE_DIVIDE(SUM(Revenue), COUNT(DISTINCT TransactionNumber)), 2) AS AverageRevenuePerInvoice
 FROM `sales-of-fmcg-stores.salesfmcg.vw_sales_analysis`
 GROUP BY SalesPersonID
 ORDER BY TotalSalesRevenue DESC;
