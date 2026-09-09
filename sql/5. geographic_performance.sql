df1196c506731df60ee3fd78b85c65180129ccf0
PHAM PHUONG THY <phamphuongthy2005@gmail.com>
2026-09-09 10:41:35 +0700

    Refactor SQL joins and simplify revenue calculations

--- sql/5. geographic_performance.sql
+++ sql/5. geographic_performance.sql
@@ -12,47 +12,24 @@ WITH city_stats AS (
         SUM(v.Revenue) AS TotalRevenue,
         SUM(v.Quantity) AS TotalQuantity
     FROM `sales-of-fmcg-stores.salesfmcg.vw_sales_analysis` AS v
-    JOIN `sales-of-fmcg-stores.salesfmcg.customers` AS cu
-        ON v.CustomerID = cu.CustomerID
-    JOIN `sales-of-fmcg-stores.salesfmcg.cities` AS ci
-        ON cu.CityID = ci.CityID
-    JOIN `sales-of-fmcg-stores.salesfmcg.countries` AS co
-        ON ci.CountryID = co.CountryID
-    GROUP BY
-        co.CountryID,
-        co.CountryName,
-        ci.CityID,
-        ci.CityName
+    JOIN `sales-of-fmcg-stores.salesfmcg.customers` AS cu ON v.CustomerID = cu.CustomerID
+    JOIN `sales-of-fmcg-stores.salesfmcg.cities` AS ci ON cu.CityID = ci.CityID
+    JOIN `sales-of-fmcg-stores.salesfmcg.countries` AS co ON ci.CountryID = co.CountryID
+    GROUP BY co.CountryID, co.CountryName, ci.CityID, ci.CityName
 )
+
 SELECT
     CountryID,
     CountryName,
-    CtyID,
+    CityID,
     CityName,
     NumberOfCustomers,
     NumberOfInvoices,
     ROUND(TotalRevenue, 2) AS TotalRevenue,
     TotalQuantity,
-    ROUND(
-        SAFE_DIVIDE(
-            TotalRevenue,
-            NumberOfCustomers
-        ), 2
-    ) AS AverageRevenuePerCustomer,
-    ROUND(
-        SAFE_DIVIDE(
-            TotalRevenue,
-            NumberOfInvoices
-        ), 2
-    ) AS AOV,
-    ROUND(
-        SAFE_DIVIDE(
-            TotalRevenue * 100.0,
-            SUM(TotalRevenue) OVER ()
-        ), 2
-    ) AS TotalRevenueContributionPercentage,
-    RANK() OVER (
-        ORDER BY TotalQuantity DESC
-    ) AS ConsumptionVolumeRank
+    ROUND(SAFE_DIVIDE(TotalRevenue, NumberOfCustomers), 2) AS AverageRevenuePerCustomer,
+    ROUND(SAFE_DIVIDE(TotalRevenue, NumberOfInvoices), 2) AS AOV,
+    ROUND(SAFE_DIVIDE(TotalRevenue * 100.0, SUM(TotalRevenue) OVER ()), 2) AS TotalRevenueContributionPercentage,
+    RANK() OVER (ORDER BY TotalQuantity DESC) AS ConsumptionVolumeRank
 FROM city_stats
 ORDER BY TotalRevenue DESC;
